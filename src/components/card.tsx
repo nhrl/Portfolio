ecd8bd81252890f686217477a504a24a80b7149f
Nhoriel <114849106+nhrl@users.noreply.github.com>
2025-03-06 13:50:53 +0800

    Update card.tsx

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -8,8 +8,8 @@ type CardProps = {
 const Card: React.FC<CardProps> = ({image, title}) => {
     return (
     <div className="h-[auto] w-[300px] lg:w-[450px]">
-        <div className="flex justify-center">
-            <img className="h-auto w-auto [@media(max-width:360px)]:h-40" loading="lazy" src={image} alt="project_image" />
+        <div className="flex justify-center h-60">
+            <img className="h-auto w-auto [@media(max-width:360px)]:h-40 object-contain" loading="lazy" src={image} alt="project_image" />
         </div>
         <div className="text-white text-[clamp(1.3rem,2cqw,2rem)] font-bold flex justify-center">
             <h2>{title}</h2>
