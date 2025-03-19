499df473f2e6885045341f748d9728318279cabc
nhrl <nhorielbalenzua1@gmail.com>
2025-03-19 15:15:16 +0800

    added chatbot

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -2,13 +2,19 @@ import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import Works from "./works";
 import Home from "./home";
+import Chat from "../components/chat"
 import { IoSunnyOutline, IoMoonOutline, IoLocationOutline } from "react-icons/io5";
 import profile from "../assets/pictures/profile.jpg";
+import { IoChatbubblesOutline } from "react-icons/io5";
 
 function Portfolio() {
   const [darkMode, setDarkMode] = useState(true);
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisibility = () => setVisible(!visible);
 
   useEffect(() => {
+
     if (darkMode) {
       document.documentElement.classList.add("dark");
     } else {
@@ -78,7 +84,17 @@ function Portfolio() {
           <Home />
           <Works />
         </div>
+        <div onClick={toggleVisibility} className="flex justify-center items-center bottom-3 right-2 md:bottom-5 md:right-5 rounded-full fixed w-13 h-13 cursor-pointer dark:bg-[#1b2535]">
+          <IoChatbubblesOutline size={25}/>           
+        </div>
+        { visible && (
+          <div className="w-5/6 fixed flex justify-end right-5 bottom-20 h-[35rem]">
+            <Chat />
+          </div>
+        )}
+        
       </motion.div>
+                      
     </div>
   );
 }
